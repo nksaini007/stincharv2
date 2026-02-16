6920926f59750216a59ed22072041f1d02386e8a
nksaini007 <ndmark007@gmail.com>
2026-02-16 22:32:47 +0530

    create react app

---
+++ stinchar/frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <p>
+      chlo kuch tufani krte h 
+     </p>
+    </>
+  )
+}
+
+export default App
